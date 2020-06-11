e446de5d81b47976511351b0787a79060a6d9bff
Christopher <66002551+MythicFrost@users.noreply.github.com>
2020-06-11 20:23:16 +1000

    Basic framework

---
+++ index.php
@@ -0,0 +1,8 @@
+<html> <!-- maybe html tags should be included in the head / footer tags -->
+<!-- head import -->
+<!-- header import -->
+
+<!-- page import (this includes the body tags) -->
+
+<!-- footer import -->
+</html>
