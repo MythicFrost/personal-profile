e94d999c6670218a1abf60c59684e7764e86d80f
Christopher <66002551+MythicFrost@users.noreply.github.com>
2020-06-19 04:30:47 +1000

    Updates

--- scripts.js
+++ scripts.js
@@ -1,2 +1,41 @@
-//var test = document.body.getElementsByClassName('nav');
-console.log("test");
+var scrollLastDate = new Date();
+var animationElements = document.getElementsByClassName("animation-fade-in-slide-up");
+
+
+
+
+window.addEventListener("DOMContentLoaded", function(e) {
+    triggerAnimations();
+});
+
+window.addEventListener("scroll", function(e) {
+    var now = new Date();
+
+    var timeDifferenceMS = now - scrollLastDate;
+
+    if (timeDifferenceMS < 125) { //wait at least 1/8th of a second per update
+        return;
+    }
+
+    triggerAnimations();
+
+    scrollLastDate = new Date();
+});
+
+function triggerAnimations() {
+    var scrollY = window.scrollY;
+    var winHeight = window.innerHeight;
+
+    for (var i = 0; i < animationElements.length; i++) {
+        var element = animationElements[i];
+        var elementY = element.offsetTop;
+
+        var difference = elementY - winHeight;
+
+        if (scrollY >= difference) {
+            if (!element.classList.contains("animation-triggered")) {
+                element.classList.add("animation-triggered");
+            }
+        }
+    }
+}
